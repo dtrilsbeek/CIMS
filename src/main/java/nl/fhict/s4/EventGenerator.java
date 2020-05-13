fc645070f7bd1b991444349874f8778a8e245e2a
Jake <jakevanhout@outlook.com>
2020-05-13 15:57:09 +0200

    fix transaction timeout in event generator
    Fixed transaction timeout in event generator.

--- src/main/java/nl/fhict/s4/EventGenerator.java
+++ src/main/java/nl/fhict/s4/EventGenerator.java
@@ -1,40 +1,58 @@
 package nl.fhict.s4;
 
+import java.time.Duration;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Flowable;
+import io.smallrye.mutiny.Multi;
 import nl.fhict.s4.models.EventModel;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.transaction.Transactional;
+import javax.transaction.UserTransaction;
 
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import org.reactivestreams.Publisher;
+
 
 
 @ApplicationScoped
 public class EventGenerator {
     private final Random random = new Random();
 
-    @Transactional
-    @Outgoing("generated-event")
-    public Flowable<EventModel> generate() {
+    @Inject UserTransaction transaction;
+    
+   @Outgoing("generated-event")
+    public Publisher<EventModel> generate() {
 
-        return Flowable.interval(10, TimeUnit.SECONDS)
-                .map(tick -> createEvent() );
+       return Multi.
+            createFrom()
+            .ticks()
+            .every(Duration.ofSeconds(1))
+            .map(_ -> createEvent())
+            .transform()
+            .byFilteringItemsWith(m -> m != null);
     }
 
-    private EventModel createEvent(){
+    private EventModel createEvent() {
+        try {
+            transaction.begin();
+            EventModel model = new EventModel(
+                    51.2 + random.nextDouble() * (51.52 - 51.2),
+                    5.18 + random.nextDouble() * (5.82 - 5.18),
+                    random.nextInt(7) + 1,
+                    "description");
 
-        EventModel model = new EventModel(
-                51.2 + random.nextDouble() * (51.52 - 51.2),
-                5.18 + random.nextDouble() * (5.82 - 5.18),
-                random.nextInt(7) + 1,
-                "description");
+            model.persist();
+            transaction.commit();
 
-        model.persist();
-
-        return model;
+            return model;
+        }
+        catch(Exception e) {
+            return null;   
+        }
     }
 
 }
