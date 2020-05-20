d37149f3eee7ff702fec52ec129ac1831d7d0b4c
Jake <jakevanhout@outlook.com>
2020-05-20 14:22:44 +0200

    add login page and add it to routes

--- webapp/src/routes.js
+++ webapp/src/routes.js
@@ -1,9 +1,11 @@
 import Home from './components/Home.vue'
 import Teams from './components/Teams.vue'
 import CimsMap from './components/Map.vue'
+import Login from './components/Login.vue'
 
 export default [
     {path: '/', component: Home},
     {path: '/map', component: CimsMap},
     {path: '/teams', component: Teams},
+    {path: '/login', component: Login},
 ];
