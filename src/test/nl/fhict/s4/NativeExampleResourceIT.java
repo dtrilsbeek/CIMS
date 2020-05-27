26cce21a8f42ec53cffc8c43bbc7cd2b36b48e63
Jake <jakevanhout@outlook.com>
2020-05-28 00:07:54 +0200

    remove example tests
    The example tests were preventing the test suite from completing.

--- src/test/nl/fhict/s4/NativeExampleResourceIT.java
+++
@@ -1,9 +0,0 @@
-package nl.fhict.s4;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeExampleResourceIT extends ExampleResourceTest {
-
-    // Execute the same tests but in native mode.
-}
