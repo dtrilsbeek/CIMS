58527715b9ed2de0d12a653903b1eff2c1ff7b91
Jake <jakevanhout@outlook.com>
2020-05-29 10:21:46 +0200

    remove old login page and login link

--- webapp/src/routes.js
+++ webapp/src/routes.js
@@ -2,7 +2,6 @@ import Home from './components/Home.vue'
 import CimsMap from './components/Map.vue'
 import Teams from './components/Teams.vue'
 import Units from "@/components/Units";
-import Login from './components/Login.vue'
 import EditUnit from './components/EditUnit.vue';
 
 export default [
@@ -10,6 +9,5 @@ export default [
     {path: '/map', component: CimsMap},
     {path: '/teams', component: Teams},
     {path: '/units', component: Units},
-    {path: '/login', component: Login},
     {path: '/editunit', component: EditUnit},
 ];
