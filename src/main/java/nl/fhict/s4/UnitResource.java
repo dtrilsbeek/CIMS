2ab2354fc3619d78e469cb4ed6487d34fb59ba46
wossnameGitHub <423368@student.fontys.nl>
2020-06-03 12:36:55 +0200

    working tests for Units

--- src/main/java/nl/fhict/s4/UnitResource.java
+++ src/main/java/nl/fhict/s4/UnitResource.java
@@ -6,6 +6,7 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import io.quarkus.panache.common.Parameters;
 import nl.fhict.s4.models.Team;
 import nl.fhict.s4.models.Unit;
 
@@ -14,7 +15,8 @@ import nl.fhict.s4.models.Unit;
 public class UnitResource {
     @DELETE
     @Path("{id}")
-    public Response deleteUnit(@PathParam("id") int id) {
+    @Transactional
+    public Response deleteUnit(@PathParam("id") Long id) {
         //TODO: HANDLE NULL?
         //TODO: STATUS AND RETURN VALUE OF DELETE?
         Unit.findById(id).delete();
@@ -26,6 +28,16 @@ public class UnitResource {
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Transactional
     public Response addUnit(@FormParam("name") String name, @FormParam("teamId") Long teamId) {
+
+        if(Unit.count("name = :name", Parameters.with("name", name)) > 0) {
+            //return a conflict response if there is a unit with the same name
+            return Response.status(409).build();
+        }
+        else if(Team.count("id = :id", Parameters.with("id", teamId)) == 0) {
+            //return a conflict response if the given team does not exist
+            return Response.status(409).build();
+        }
+
         Unit unit = Unit.addUnit(name, teamId);
 
         return Response.ok(unit).build();
