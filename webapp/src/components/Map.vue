40a28f79f30062190df480f60ed3440eae7157b1
dtrilsbeek <d.trilsbeek@gmail.com>
2020-06-03 11:42:25 +0200

    add user instruction on map load

--- webapp/src/components/Map.vue
+++ webapp/src/components/Map.vue
@@ -1,132 +1,148 @@
 <template>
-  <div class="grid">
-      <region-menu :bus="bus" v-on:region-bounds="setInitialPosition($event)" v-on:move-to="moveTo($event)"></region-menu>
-      <div id="map"></div>
-      <div class="map-info">
+    <div class="grid">
+        <region-menu :bus="bus" v-on:region-bounds="setInitialPosition($event)"
+                     v-on:move-to="moveTo($event)"></region-menu>
+        <div id="map"></div>
+        <div class="map-info">
             <h4>Info</h4>
-      </div> 
-  
-      <aside class="info">
-            <home :bus="bus" />        
-          <active-events :bus="bus" v-on:move-to-event="moveToEvent($event)" />
-     
-      </aside>
+        </div>
 
-  </div>
+        <aside class="info">
+            <home :bus="bus"/>
+            <active-events :bus="bus" v-on:move-to-event="moveToEvent($event)"/>
+
+        </aside>
+
+    </div>
 </template>
 
 <script>
-import CimsMap from '@/components/leaflet/CimsMap'
-import CimsMarker from '@/components/leaflet/CimsMarker'
-import CimsRectangle from '@/components/leaflet/CimsRectangle';
-// Css for loading the map smoothly
-import 'leaflet/dist/leaflet.css'
-import Home from './Home.vue'
-import RegionMenu from '@/components/RegionMenu'
-import ActiveEvents from './ActiveEvents'
-import Vue from 'vue';
-import config from '@/components/rest/RestConfig'
+    import CimsMap from '@/components/leaflet/CimsMap'
+    import CimsMarker from '@/components/leaflet/CimsMarker'
+    import CimsRectangle from '@/components/leaflet/CimsRectangle';
+    // Css for loading the map smoothly
+    import 'leaflet/dist/leaflet.css'
+    import Home from './Home.vue'
+    import RegionMenu from '@/components/RegionMenu'
+    import ActiveEvents from './ActiveEvents'
+    import Vue from 'vue';
+    import config from '@/components/rest/RestConfig'
+    import L from "leaflet";
 
     export default {
-    components: {
-        home: Home,
-        regionMenu: RegionMenu,
-        activeEvents: ActiveEvents
-    },
-    data() {
-        return{
-            // eventStream: null,
-            streamUrl: config.getUrl('events', 'stream'),
-            bus: new Vue(),
-            leafletMap: null,
-            markers: [],
-            initialPosition: null,
-            // fontys: [51.451069, 5.4772183],
-            eventSource: null,
-        }
-    },
-
-    mounted() {
-        this.bus.$emit("retrieve-current-region-bounds");            
-        this.createEventSource();
-    },
-
-    methods: {
-
-        setSelectedMarker(marker) {
-            if (this.selectedMarker) {
-                this.selectedMarker.getElement().classList.remove('active');
-            }
-            this.selectedMarker = marker;
+        components: {
+            home: Home,
+            regionMenu: RegionMenu,
+            activeEvents: ActiveEvents
         },
-
-        getIconTypeString(type) {
-            switch (type) {
-                case 1:
-                    return "fireTruck"
-                case 2:
-                    return "ambulance"
-                case 3:
-                    return "fire"
-                case 4:
-                    return "police"
-                default:
-                    return "ambulance"
+        data() {
+            return {
+                // eventStream: null,
+                streamUrl: config.getUrl('events', 'stream'),
+                bus: new Vue(),
+                leafletMap: null,
+                markers: [],
+                initialPosition: null,
+                // fontys: [51.451069, 5.4772183],
+                eventSource: null,
             }
         },
 
-        createEventSource() {
-            this.eventSource = new EventSource("http://localhost:8083/events/stream");
-            this.eventSource.onmessage = (event) => {
+        mounted() {
+            this.bus.$emit("retrieve-current-region-bounds");
+            this.createEventSource();
+        },
 
-                const data = JSON.parse(event.data);
-                const type = this.getIconTypeString(data.type);
+        methods: {
 
-                if (data.isUpdate) {
-                    const marker = this.markers[data.id]
-                    if(marker) {
-                        marker.type = data.type;
-                        marker.description = data.description;
-                        marker.moveTo([data.lat, data.lon], 1500);
-                    }
-                } else {
-                    this.markers[data.id] = new CimsMarker(this, data.id, type, data.description, [data.lat, data.lon]);
+            setSelectedMarker(marker) {
+                if (this.selectedMarker) {
+                    this.selectedMarker.getElement().classList.remove('active');
                 }
-            };
-        },
+                this.selectedMarker = marker;
+            },
 
-        setInitialPosition(bounds) {
-            //convert to number
-            let numberBounds = [
-                Array.from(bounds[0], x => +x),
-                Array.from(bounds[1], x => +x)
-            ]
+            getIconTypeString(type) {
+                switch (type) {
+                    case 1:
+                        return "fireTruck"
+                    case 2:
+                        return "ambulance"
+                    case 3:
+                        return "fire"
+                    case 4:
+                        return "police"
+                    default:
+                        return "ambulance"
+                }
+            },
 
-            //avg middle position of region bounds
-            let lat = (numberBounds[0][0] + numberBounds[1][0]) / 2;
-            let lon = (numberBounds[0][1] + numberBounds[1][1]) / 2;
+            createEventSource() {
+                this.eventSource = new EventSource("http://localhost:8083/events/stream");
+                this.eventSource.onmessage = (event) => {
 
-            this.initialPosition = [lat, lon];
-            
-            this.leafletMap = new CimsMap(this.initialPosition, 13);
-            this.leafletMap.on('click', (e) => {
-                const selected = this.selectedMarker ? this.selectedMarker.id : null;
+                    const data = JSON.parse(event.data);
+                    const type = this.getIconTypeString(data.type);
 
-                this.$root.$refs.home.show(selected, e.latlng);
-            });      
-        },
+                    if (data.isUpdate) {
+                        const marker = this.markers[data.id]
+                        if (marker) {
+                            marker.type = data.type;
+                            marker.description = data.description;
+                            marker.moveTo([data.lat, data.lon], 1500);
+                        }
+                    } else {
+                        this.markers[data.id] = new CimsMarker(this, data.id, type, data.description, [data.lat, data.lon]);
+                    }
+                };
+            },
 
-        moveTo(bounds){
-            this.leafletMap.flyToBounds(bounds);
-            new CimsRectangle(bounds, 'blue', 1).addTo(this.leafletMap);
-            this.bus.$emit("retrieve-by-bounds", bounds);            
-        },
-        moveToEvent(event) {
-            this.leafletMap.flyTo([event.lat, event.lon], 12);
-            this.markers[event.id].fire('click');
+            setInitialPosition(bounds) {
+                //convert to number
+                let numberBounds = [
+                    Array.from(bounds[0], x => +x),
+                    Array.from(bounds[1], x => +x)
+                ]
+
+                //avg middle position of region bounds
+                let lat = (numberBounds[0][0] + numberBounds[1][0]) / 2;
+                let lon = (numberBounds[0][1] + numberBounds[1][1]) / 2;
+
+                this.initialPosition = [lat, lon];
+
+                this.leafletMap = new CimsMap(this.initialPosition, 13);
+                this.showUserInstruction(lat, lon);
+                this.leafletMap.on('click', (e) => {
+                    const selected = this.selectedMarker ? this.selectedMarker.id : null;
+
+                    this.$root.$refs.home.show(selected, e.latlng);
+                });
+            },
+
+            showUserInstruction(lat, lon) {
+                var imageUrl = '/userInstruction.png';
+                var imageBounds = [[lat-0.05, lon-0.14], [lat + 0.05, lon + 0.14]];
+                var overlay = L.imageOverlay(imageUrl, imageBounds);
+                overlay.addTo(this.leafletMap);
+                setTimeout(function() {
+                    overlay.getElement().classList.add('fadeout')
+                    setTimeout(function() {
+                        overlay.remove(this.leafletMap);
+                    }, 800);
+                }, 3000)
+            },
+
+            moveTo(bounds) {
+                this.leafletMap.flyToBounds(bounds);
+                new CimsRectangle(bounds, 'blue', 1).addTo(this.leafletMap);
+                this.bus.$emit("retrieve-by-bounds", bounds);
+            },
+            moveToEvent(event) {
+                this.leafletMap.flyTo([event.lat, event.lon], 12);
+                this.markers[event.id].fire('click');
+            }
         }
     }
-}
 </script>
 
 <!--suppress HtmlUnknownTarget -->
